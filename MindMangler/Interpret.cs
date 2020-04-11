60845fabc88adaf7197194c1c1d09ba27d7fc828
cainy-a <cainatkinson98@gmail.com>
2020-04-11 14:58:16 +0100

    about to implement commands.

---
+++ MindMangler/Interpret.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MindMangler
+{
+	internal partial class Program
+	{
+		public static void InterpreterMain(string code, bool verboseOn)
+		{
+			List<int> memoryBlock = new List<int>(30000); // Brainfuck memory cells
+			List<char> commands = code.ToList(); // Get a List<char> of characters
+
+			int counter = 0;
+			do
+			{
+				counter++;
+				switch (commands[counter].ToString())
+				{
+					default:
+						break;
+				}
+			} while (counter < code.Length);
+		}
+	}
+}
--- MindMangler/Program.cs
+++ MindMangler/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace MindMangler
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace MindMangler
 {
-  internal class Program
+  internal partial class Program
   {
     public static void Main(string[] args)
     {
+      bool verbose = false;
       
+      Console.WriteLine("MindMangler by Cain Atkinson. (https://cainy-a.github.io)\nA Brainfuck interpreter written in C#");
+      if (args.Contains("-v") | args.Contains("--verbose"))
+      {
+        verbose = true;
+      }
+      if (args.Contains("--help") | args.Contains("-h") | args.Length == 0)
+      {
+        Console.WriteLine("Mindmangle [filepath] [options]\nPlease add a file path.\n-v or --verbose makes it tell you if a non-brainfuck instruction was encountered (not recommended at all).");
+        return;
+      }
+      if (args.Length > 2)
+      {
+        Console.WriteLine("Please use less arguments");
+        return;
+      }
+      string filePath = args[0];
+      StreamReader streamReader = new StreamReader(filePath, Encoding.UTF8);
+      string codeToRun;
+      try
+      {
+        codeToRun = streamReader.ReadToEnd();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("That's not a valid path.");
+        throw;
+        return;
+      } // Read the passed in file to a variable.
+      
+      InterpreterMain(codeToRun, verbose);
     }
+
+    /*/// <summary>
+    /// Turns a string into a List<char>
+    /// </summary>
+    public static List<char> StringDeconstructor(string input)
+    {
+      List<char> toReturn = new List<char>();
+      foreach (var letter in input)
+      {
+        toReturn.Add(letter);
+      }
+      return toReturn;
+    }*/
   }
 }
